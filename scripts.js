b7416cdc63feda46f532d6a2dc35a60a89758ef0
Ivaav <mauricio.v27@outlook.com>
2024-02-27 21:31:49 -0300

    projeto completo

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function selectRan() {    
+    document.getElementById('Ran').classList.toggle('selected')
+}
+function selectMiki() {    
+    document.getElementById('Miki').classList.toggle('selected')
+}
+function selectSu() {    
+    document.getElementById('Su').classList.toggle('selected')
+}
