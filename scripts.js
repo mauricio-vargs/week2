bcfd254a928fbed1eebdf3a2119c2112c4cf46cd
Ivaav <mauricio.v27@outlook.com>
2024-02-29 00:11:47 -0300

    Adição do botão de informações

--- scripts.js
+++ scripts.js
@@ -1,9 +1,19 @@
-function selectRan() {    
+function infoRan() {    
     document.getElementById('Ran').classList.toggle('selected')
 }
-function selectMiki() {    
+function infoMiki() {    
     document.getElementById('Miki').classList.toggle('selected')
 }
-function selectSu() {    
+function infoSu() {    
     document.getElementById('Su').classList.toggle('selected')
+}
+
+function selectRan() {
+    alert("Você selecionou a Ran!")
+}
+function selectMiki() {
+    alert("Você selecionou a Miki!")
+}
+function selectSu() {
+    alert("Você selecionou a Su")
 }
